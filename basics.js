ea0df6e4a9110c954e123a2b2c4aa7f7138ffb06
Aman_kori <amankori2025@gmail.com>
2025-06-07 10:31:38 +0000

    learn about string & math properties (rename folder)

--- basics.js
+++ basics.js
@@ -77,8 +77,8 @@
 // Symbol                 "symbol"
 
 
-// const name = "Aman"
-// const repoCount = 50
+const name = "Aman"
+const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
@@ -91,9 +91,9 @@
 // Primitive data types are stored in stack memory.
 // Example:
 
-// let myName = "Aman";
-// let anotherName = myName;
-// anotherName = "Rahul";
+let myName = "Aman";
+let anotherName = myName;
+anotherName = "Rahul";
 
 // console.log(myName);      // Output: Aman
 // console.log(anotherName); // Output: Rahul
@@ -104,13 +104,13 @@
 // Non-primitive data types are stored in heap memory.
 // Example:
 
-// let userOne = {
-//     email: "user@example.com",
-//     upi: "user@ybl"
-// };
+let userOne = {
+    email: "user@example.com",
+    upi: "user@ybl"
+};
 
-// let userTwo = userOne;
-// userTwo.email = "newuser@example.com";
+let userTwo = userOne;
+userTwo.email = "newuser@example.com";
 
 // console.log(userOne.email);   // Output: newuser@example.com
 // console.log(userTwo.email);   // Output: newuser@example.com
@@ -118,3 +118,77 @@
 // Changing userTwo also changes userOne because non-primitives are copied by reference.
 
 
+// ....................string in js.................
+
+let str = new String('Aakash')
+
+// properties of String
+
+// console.log(str.length);
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+// console.log(str.charAt(2));
+// console.log(str.indexOf('s'));
+
+let gameName = new String('codewars')
+
+// console.log(gameName.substring(0, 4))
+
+// console.log(gameName.slice(-7, 4))  // the - value start with the reverse
+
+// console.log(gameName.slice(2, 4))
+
+const newString = "   aman    "
+
+// console.log(newString);              
+// console.log(newString.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+
+
+// ...................math Properties..................
+
+
+
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)

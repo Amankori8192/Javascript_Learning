721f185f1ee204a96052f662f3a67ea868d25a34
Aman_kori <amankori2025@gmail.com>
2025-06-12 18:19:54 +0000

    more about arrays

--- array.js
+++ array.js
@@ -87,10 +87,10 @@ const real_another_array = another_array.flat(Infinity)
 
 // console.log(Array.isArray("Hitesh"))             //  to check pass element is exist or not
 // console.log(Array.from("Hitesh"))               //  to convert in array
-// console.log(Array.from({name: "hitesh"}))       // return a empty array / from the object if you are try to making an array. so, you have to tell them you are trying to making an array from the keys of object or value. 
+// console.log(Array.from({name: "hitesh"}))       // (whenever unable to making an array)it's return a empty array / from the object if you are try to making an array. so, you have to tell them you are trying to making an array from the keys or value of object.
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));

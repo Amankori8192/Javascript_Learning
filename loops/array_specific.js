8ea3f0a0b5724925fdef2ea0fdbdf9585ac6c83a
Aman_kori <amankori2025@gmail.com>
2025-06-16 12:20:35 +0000

    learn about reduce method on array

--- loops/array_specific.js
+++ loops/array_specific.js
@@ -41,10 +41,10 @@ const myObject = {
 
 
 
-// ...........filter ............
+// ...........filter method............
 
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
@@ -78,4 +78,45 @@ const books = [
   userBooks = books.filter( (bk) => { 
     return bk.publish >= 1995 && bk.genre === "History"
 })
-  console.log(userBooks);
+//   console.log(userBooks);
+
+
+
+
+  // ...........reduce method............
+
+
+  const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+// console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price , 0)
+
+console.log(priceToPay);
